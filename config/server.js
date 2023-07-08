cc5234f38b8ae758c749fea18f913554e515d9d0
dimola <didiogidan@gmail.com>
2023-07-08 12:49:07 +0100

    added api keys

--- config/server.js
+++ config/server.js
@@ -1,10 +1,15 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS", [
+      "KZgt7s8WLG6w19hdbg83aQ==",
+      "8CK7TCa10SIeCX1Rixbgtg==",
+      "Yq3n4yK2fYwM4r9LaDOa7g==",
+      "FkoD3n1nlmWzy/0IJPw96A==",
+    ]),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
